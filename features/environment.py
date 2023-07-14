84daf2b441f4544fb13bbb49008edf220f2b0589
Stephen Okonkwo <stephenokonkwo97@gmail.com>
2023-07-14 08:49:21 -0400

    Initial Commit

--- features/environment.py
+++ features/environment.py
@@ -4,19 +4,46 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
+
 def browser_init(context):
-    """
-    :param context: Behave context
-    """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
-    context.driver.maximize_window()
+    # """
+    # :param context: Behave context
+    # """
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+    # context.driver.maximize_window()
+
+    context.driver = webdriver.Firefox(executable_path='/Users/stephenokonkwo/Desktop/python-selenium-automation/geckodriverr')
+    # context.driver = webdriver.Safari()
+
+    #### HEADLESS MODE ####
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(
+    #     chrome_options=options,
+    #     service=service
+    # )
+
+    #### BROWSERSTACK ####
+    # desired_cap = {
+    #     'browser': 'Firefox',
+    #     'os_version': '11',
+    #     'os': 'Windows',
+    #     'name': test_name
+    # }
+    # bs_user = ''
+    # bs_key = ''
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     # context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
+
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
